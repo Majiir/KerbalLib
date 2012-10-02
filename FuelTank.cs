b95fb9e2aca8115d0658c86e8c0d882e505696e4
Majiir Paktu <majiir@nabaal.net>
2012-10-02 19:02:48 -0400

    FuelTank parts become active again when fuel is added

---
+++ FuelTank.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace MajiirKerbalLib
+{
+    public class FuelTank : global::FuelTank
+    {
+        protected override void onPartStart()
+        {
+            this.started = true;
+            base.onPartStart();
+        }
+
+        protected override void onPartFixedUpdate()
+        {
+            if ((this.state == PartStates.DEACTIVATED) && (this.fuel > 0))
+            {
+                this.state = PartStates.ACTIVE;
+                if (this.stackIcon != null)
+                {
+                    this.stackIcon.SetIconColor(XKCDColors.BrightTeal);
+                }
+                else
+                {
+                    MonoBehaviour.print("[MajiirKerbalLib] StackIcon was null!");
+                }
+                this.DrainFuel(0);
+            }
+            base.onPartFixedUpdate();
+        }
+    }
+}
