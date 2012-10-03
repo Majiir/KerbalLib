f67530f1b59b1e23dee68ec54bb6491f5baf2790
Majiir Paktu <majiir@nabaal.net>
2012-10-03 01:18:21 -0400

    RCSModule parts can re-enable when fuel becomes available

---
+++ RCSModule.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace MajiirKerbalLib
+{
+    public class RCSModule : global::RCSModule
+    {
+        protected override void onPartFixedUpdate()
+        {
+            if (this.vessel.rootPart.RequestRCS(0, 0))
+            {
+                this.stackIcon.SetIconColor(XKCDColors.White);
+            }
+            else
+            {
+                this.stackIcon.SetIconColor(XKCDColors.SlateGrey);
+                base.onCtrlUpd(new FlightCtrlState());
+            }
+            base.onPartFixedUpdate();
+        }
+    }
+}
