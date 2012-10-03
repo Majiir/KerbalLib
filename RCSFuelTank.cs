c541fb27227fd935fbaec50cbf35584f84436609
Majiir Paktu <majiir@nabaal.net>
2012-10-03 00:11:57 -0400

    RCSFuelTank parts become active again when fuel is added

---
+++ RCSFuelTank.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace MajiirKerbalLib
+{
+    public class RCSFuelTank : global::RCSFuelTank
+    {
+        protected override void onPartStart()
+        {
+            this.started = true;
+            base.onPartStart();
+        }
+
+        protected override void onPartFixedUpdate()
+        {
+            if ((this.state == PartStates.DEACTIVATED) && (this.fuel > 0))
+            {
+                this.state = PartStates.ACTIVE;
+                if (this.stackIcon != null)
+                {
+                    this.stackIcon.SetIconColor(XKCDColors.LightPeriwinkle);
+                }
+                else
+                {
+                    MonoBehaviour.print("[MajiirKerbalLib] StackIcon was null!");
+                }
+                this.getFuel(0);
+            }
+            base.onPartFixedUpdate();
+        }
+    }
+}
